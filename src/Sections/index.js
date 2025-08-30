623403b9d231619ded127f20b24fad1ac15e546b
adityagupta2oo4 <ag751000@gmail.com>
2025-08-30 13:51:04 +0000

    Done with Nav-Bar and Using taiwind v3

---
+++ src/Sections/index.js
@@ -0,0 +1,22 @@
+import Hero from "./Hero";
+import PopularProduct from "./PopularProduct";
+import SuperQuality from "./SuperQuality";
+import Services from "./Services";
+import Footer from "./Footer";
+import SpecialOffer from "./SpecialOffer";
+import CustomerReviews  from "./CustomerReviews";
+import Subscribe from "./Subscribe";
+
+export{
+    Hero,
+    PopularProduct,
+    SuperQuality,
+    Services,
+    SpecialOffer,
+    CustomerReviews,
+    Subscribe,
+    Footer,
+
+}
+
+// just to make our App.jsx more clean
